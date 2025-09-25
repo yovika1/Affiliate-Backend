5cc74434c14261194a167a4d38374409739f3862
yovika1 <yovikaranga@gmail.com>
2025-09-26 03:34:24 +0530

    update cors

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app = express()
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: "*",
+   methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(blogRoutes)
 app.use(coupenRoutes)

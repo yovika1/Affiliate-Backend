8efefed3827ef976bfc134ef0550e932e62c853f
yovika1 <yovikaranga@gmail.com>
2025-12-18 00:50:12 +0530

    update fetchProduct

--- models/Product.js
+++ models/Product.js
@@ -5,13 +5,14 @@ const productSchema = new mongoose.Schema(
     productName: { type: String, required: true },
     imageUrl: { type: String, default: "" },
     productUrl: { type: String, required: true },
+    affiliateUrl: { type: String, required: true },
 
     currentPrice: { type: Number, default: null },
     originalPrice: { type: Number, default: null },
-    discount: { type: String, default: null },
+    // discount: { type: String, default: null },
 
-    rating: { type: Number, default: null },
-    reviewsCount: { type: Number, default: null },
+    // rating: { type: Number, default: null },
+    // reviewsCount: { type: Number, default: null },
 
     platform: { type: String, enum: ["flipkart", "mamaearth", "amazon", "ajio", "myntra","unknown"],
        required: true,
